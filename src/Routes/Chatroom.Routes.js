516d5149aba3d0ddaae3742e88e106297326dee2
Ayush singh <aayushssingh2003@gmail.com>
2025-09-13 17:40:45 +0530

    add chatroom controller

---
+++ src/Routes/Chatroom.Routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { Router } from "express";
+import Auth from "../Middleware/Auth.middleware.js";
+import {createchatroom} from "../Controllers/Chatroom.controllers.js";
+const router = Router();
+
+// Create Chatroom route 
+
+router.route('/').post(Auth , createchatroom);
+
+export default router;
