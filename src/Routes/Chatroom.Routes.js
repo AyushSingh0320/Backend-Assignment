1129f891113bfdb264098baca7d08eb6e79213ff
Ayush singh <aayushssingh2003@gmail.com>
2025-09-15 11:35:09 +0530

    add modelcheck middleware

--- src/Routes/Chatroom.Routes.js
+++ src/Routes/Chatroom.Routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Router } from "express";
 import Auth from "../Middleware/Auth.middleware.js";
+import modelcheck from '../Middleware/modelcheck.middleware.js';
 import {createchatroom , getchatrooms, getchatroomdata , postmessage} from "../Controllers/Chatroom.controllers.js";
 const router = Router();
 
@@ -14,6 +15,6 @@ router.route("/").get(Auth , getchatrooms);
 router.route("/:Id").get(Auth , getchatroomdata);
 
 // Post Message route
-router.route("/:id/messages").post(Auth , postmessage);
+router.route("/:id/messages").post(Auth , modelcheck , postmessage);
 
 export default router;
