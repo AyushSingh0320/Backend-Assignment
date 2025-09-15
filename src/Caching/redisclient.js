c7c602a2b05da4d1d84ff5dd4d8cb13ca94f1ddc
Ayush singh <aayushssingh2003@gmail.com>
2025-09-15 11:05:11 +0530

    add modelcheck middleware

--- src/Caching/redisclient.js
+++ src/Caching/redisclient.js
@@ -1,20 +1,11 @@
 import { createClient } from "redis";
 import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: "../../.env" });
 
 const client = createClient({
-  host: process.env.REDIS_HOST || "localhost",
-  port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD || undefined,
+ url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`,
+password: process.env.REDIS_PASSWORD || undefined,
 })
-
-client.on("error", (err) => {
-  console.error("Redis Client Error", err);
-});
-
-client.on("connect", () => {
-  console.log("Connected to Redis");
-});
 
 
 export default client;
